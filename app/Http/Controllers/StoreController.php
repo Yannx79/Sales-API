1291e444f107be9607b050bb1ae4b564cb984704
Yannick <yannfscz@gmail.com>
2024-06-08 09:14:32 -0500

    feat (models): relationships & scope local ended

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -17,14 +17,6 @@ class StoreController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreStoreRequest $request)
@@ -36,14 +28,6 @@ class StoreController extends Controller
      * Display the specified resource.
      */
     public function show(Store $store)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Store $store)
     {
         //
     }
